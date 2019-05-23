84509d752400de30c1070cf6055fb83bec866cb6
Leonardo Motta <leo@domboscoatacado.com.br>
2019-05-23 14:55:30 -0300

    Test /v1/auth/signup

---
+++ src/__tests__/auth.test.js
@@ -0,0 +1,55 @@
+const app = require('../app');
+const User = require('../models/user');
+const request = require('supertest');
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://localhost:27017/vuttrtest';
+
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+describe('Auth test', () => {
+  beforeEach(done => {
+    for (let i in mongoose.connection.collections) {
+      mongoose.connection.collections[i].deleteOne({});
+    }
+
+    return done();
+  });
+
+  afterAll(done => {
+    mongoose.connection.close();
+    return done();
+  });
+
+  describe('test /v1/auth/... routes', () => {
+    describe('/signup', () => {
+      it('sign up a user', async () => {
+        await request(app)
+          .post('/v1/auth/signup')
+          .send({ name: 'Leo', lastName: 'Motta', email: 'test2@test.com', password: '123456' })
+          .expect(201);
+      });
+
+      it('refuse user already created', async () => {
+        const user = new User({
+          name: 'Diane',
+          lastName: 'Castro',
+          email: 'test@test.com',
+          password: '123456'
+        });
+        await user.save();
+
+        await request(app)
+          .post('/v1/auth/signup')
+          .send({ name: 'Diane', lastName: 'Castro', email: 'test@test.com', password: '123456' })
+          .expect(400);
+      });
+
+      it('missing field', async () => {
+        await request(app)
+          .post('/v1/auth/signup')
+          .send({ name: 'Leo' })
+          .expect(400);
+      });
+    });
+  });
+});
